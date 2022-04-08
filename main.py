cc9f483a7076a3de70ed9f2f8ec54bbdb3942463
Lautaron Landin <llandin@licenciasonline.com>
2022-04-08 02:09:49 -0300

    field and document

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 from pydoc import describe
+from enum import Enum
+
 from turtle import title
 from typing import Optional
-from unittest.util import _MAX_LENGTH
 from pydantic import BaseModel
+from pydantic import Field
 from fastapi import FastAPI, Body, Query, Path
 
 
 app = FastAPI()
 
+class HairColor(Enum):
+    white= "white"
+    brown = "brown"
+    black = "black"
+    blonde = "blonde"
+
 class Location(BaseModel):
     city: str
-    country:str
-    state:str
+    country: str
+    state: str
+
 
 class Person(BaseModel):
-    first_name: str
-    last_name: str
-    age: int
-    hair_color: Optional[str] = None
+    first_name: str = Field(..., min_length=1, max_length=50)
+    last_name: str = Field(..., min_length=1, max_length=50)
+    age: int = Field(..., gt=0, le=115)
+    hair_color: Optional[HairColor] = Field(default=None)
     is_married: Optional[bool] = None
 
 
@@ -26,10 +35,11 @@ def home():
     return {"hello": "world"}
 
 
-#request and response body
+# request and response body
 @app.post("/person/new")
 def create_person(person: Person = Body(...)):
     return person
+
 
 @app.get("/person/detail")
 def show_person(
@@ -39,15 +49,16 @@ def show_person(
         max_length=50,
         title="Person Name",
         description="this is the persona name"
-        ),
+    ),
     age: Optional[int] = Query(
         ...,
         title="Person age",
         description="this is the person age"
-        )
+    )
 ):
     return {name: age}
- 
+
+
 @app.get("/person/detail/{person_id}")
 def show_person(
     person_id: int = Path(
@@ -55,15 +66,16 @@ def show_person(
         gt=0,
         title="identity person id",
         description="this is id the person"
-        )
+    )
 ):
     return {person_id: "existe"}
+
 
 @app.put("/person/{person_id}")
 def update_person(
     person_id: int = Path(
         ...,
-        title = "person id",
+        title="person id",
         description="this is the person id",
         gt=0
     ),
@@ -73,15 +85,3 @@ def update_person(
     result = person.dict()
     result.update(location.dict())
     return result
-
-
-
-
-
-
-
-
-
-
-
-
